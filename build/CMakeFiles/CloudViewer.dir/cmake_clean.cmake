7087dd74101e434b7b5cebb302dc98bb0bca4dac
Tim <timiainmarsh@gmail.com>
2015-07-01 12:11:54 +0200

    cloud viewer asks for file name. must be in same dir

---
+++ build/CMakeFiles/CloudViewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloudViewer.dir/main.cpp.o"
+  "CloudViewer.pdb"
+  "CloudViewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CloudViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
